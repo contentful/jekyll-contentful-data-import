8be8b514cc61fb831b804e8c5cc8c7f4766f08c5
Adam Hardwick <adam@flxsource.org>
2016-04-23 10:24:01 +0100

    Index entries by the displayField specified by the content type
    In addition to fetching the entries, we now additionally fetch the content_type data. The serializer then uses this information to index each entry by the contents of the specified field as the index.
    
    This does require that the displayField is unique - setting it as a slug ensures this - so it would make sense for this to be an optional feature rather than default. But it allows for precise selection of an entry with requiring looping within your page. Can now use:
    
    {{ site.data.contentful.spaces.mySpace.myContentType.entryName.myField }}

--- lib/jekyll-contentful-data-import/serializer.rb
+++ lib/jekyll-contentful-data-import/serializer.rb
@@ -4,18 +4,29 @@ require 'jekyll-contentful-data-import/mappers'
 module Jekyll
   module Contentful
     class Serializer
-      attr_reader :entries, :config
+      attr_reader :entries, :content_types, :config
 
-      def initialize(entries, config = {})
+      def initialize(entries, content_types, config = {})
         @entries = entries
+        @content_types = content_types
         @config = config
       end
 
       def serialize
         result = {}
+        content_type_keys = {}
+
+        content_types.each do |content_type|
+          content_type_keys[content_type.sys[:id]] = content_type.properties[:displayField].to_sym
+        end
+
+        puts content_type_keys
+
         entries.group_by { |entry| entry.content_type.id }.each do |content_type, entry_list|
-          result[content_type] = entry_list.map do |entry|
-            ::Jekyll::Contentful::Mappers::Base.mapper_for(entry, config).map
+          key = content_type_keys[content_type]
+          result[content_type] = {} unless result.key? content_type
+          entry_list.each do |entry|
+            result[content_type][entry.fields[key]] = ::Jekyll::Contentful::Mappers::Base.mapper_for(entry, config).map
           end
         end
 
